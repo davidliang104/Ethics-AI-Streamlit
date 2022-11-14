8a0c4f1c759fb7ae95fbbda783eb3bf8963aa23f
ragequitting101 <davidliang104@gmail.com>
2022-11-14 13:03:42 +0300

    Added function to clear directories

--- app.py
+++ app.py
@@ -17,9 +17,22 @@ model = load_model(os.path.join('models', 'model_cls_810_28_700_gb_imb.h5'))
 print(f"Setup complete. Using torch {torch.__version__} ({torch.cuda.get_device_properties(0).name if torch.cuda.is_available() else 'CPU'})")
 
 image_dir = 'images'
-#'yolov5\\runs\\detect\\exp'
 det_dir = os.path.join('yolov5', 'runs', 'detect', 'exp')
 label_dir = os.path.join(det_dir, 'labels')
+
+def silent_remove(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+
+def empty_dir(dir):
+    for file in os.listdir(dir):
+        if file != '.gitkeep':
+            silent_remove(os.path.join(dir, file))
+
+# Empty all directories
+empty_dir(image_dir)
+empty_dir(det_dir)
+empty_dir(label_dir)
 
 def image_prep_cls(image):
   # Image preprocessing
@@ -28,23 +41,6 @@ def image_prep_cls(image):
   image = cv2.bilateralFilter(image,15,75,75) # Apply bilateral filter for blurring
   image = image / 255.0 # Normalizing
   return image
-
-# def predict_cls(img_dir):
-#     x_test = []
-
-#     for image_name in os.listdir(img_dir):
-#         image = cv2.imread(os.path.join(img_dir,image_name))
-#         image = image_prep_cls(image)
-#         x_test.append(image)
-#     x_test = np.array(x_test)
-
-#     prediction = model.predict(x_test)
-#     y_pred = np.argmax(prediction, axis=1) # Undo one-hot encoding
-
-#     label_names = {0:'sexual harassment', 1:'sexual abuse', 2:'sexual violence'}
-#     y_pred = np.array([label_names[x] for x in y_pred])
-
-#     return y_pred
 
 def predict_cls(image_path):
     image = cv2.imread(image_path)
@@ -60,21 +56,24 @@ def predict_cls(image_path):
 
     return output_v
 
-def img_prep_box(image):
-    # Image preprocessing
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert to greyscale
-    image = cv2.bilateralFilter(image,15,75,75) # Apply bilateral filter for blurring
-    # No need to normalize because the YOLO model already does that
-    return image
+# def img_prep_box(image):
+#     # Image preprocessing
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert to greyscale
+#     image = cv2.bilateralFilter(image,15,75,75) # Apply bilateral filter for blurring
+#     # No need to normalize because the YOLO model already does that
+#     return image
 
 def predict_box(image_dir):
-    # image = cv2.imread(image_path)
+    # Image augmentation
+    # img_list = os.listdir(image_dir)
+    # for image in img_list:
+    #     img_path = os.path.join(image_dir, image)
+    #     img_array = cv2.imread(img_path)
+    #     img_array = img_prep_box(img_array)
+    #     cv2.imwrite(img_path, img_array)
+
     # Run prediction
     subprocess.run([f"{sys.executable}", 'yolov5/detect.py', '--weights', os.path.join('models','best_iaug720.pt'), '--img', '720', '--conf', '0.4', '--source', image_dir, '--save-txt', '--save-conf', '--exist-ok'])
-
-def silent_remove(filename):
-    if os.path.exists(filename):
-        os.remove(filename)
 
 def save_uploaded_file(uploaded_files):
     file_names = []
@@ -86,7 +85,7 @@ def save_uploaded_file(uploaded_files):
     # Remove images not in uploaded files anymore
     for image in os.listdir(image_dir):
         print("File name:",image)
-        if image not in file_names:
+        if image not in file_names and image != '.gitkeep':
             silent_remove(os.path.join(image_dir, image))
             silent_remove(os.path.join(det_dir, image))
             image_name = image.split(".")
